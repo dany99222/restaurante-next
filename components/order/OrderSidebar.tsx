69848e28fa6af981378152a02a4f3976199deb5e
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-03 12:53:29 -0800

    Data fetching de categories

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,3 +1,23 @@
-export default function OrderSidebar() {
-  return <aside className="md:w-64 md:h-screen bg-orange-600">OrderSidebar</aside>;
+// Abrimos la conexion a la bd 
+import prisma from "@/src/lib/prisma"
+
+async function getCategories() {
+  return await prisma.category.findMany()
+}
+
+
+export default async function OrderSidebar() {
+  //Hacemos la consulta de lo que nos queremos traer
+    const categories = await getCategories()
+  
+    console.log(categories);
+
+  return (
+   <aside className="md:w-72 md:h-screen bg-white">
+        OrderSideBar
+        {categories.map((cat)=>(
+          <p key={cat.id}>{cat.name}</p>
+        ))}
+        </aside>
+  );
 }
