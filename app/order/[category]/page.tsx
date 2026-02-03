6d10431f40e397b78b50d2bbe78ed25cef239474
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-03 15:10:18 -0800

    Detectar categoria actual y obtener productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,8 +1,23 @@
+import prisma from "@/src/lib/prisma";
 
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
 
-export default function OrderPage({params}: {params: { category: string}}) {
-  console.log(params)
-  return (
-    <div>OrderPage</div>
-  )
+  return products;
+}
+
+export default async function OrderPage({
+  params,
+}: {
+  params: { category: string };
+}) {
+ const products = await getProducts(params.category)
+ console.log(products)
+  return <div>OrderPage</div>;
 }
