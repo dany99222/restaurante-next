681cb89d49fbc06e00bc00979f2d866b662e6f9c
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-05 11:38:40 -0800

    Avitar duplicados en el carrito

--- components/ui/Categoryicon.tsx
+++ components/ui/Categoryicon.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Category } from "@/src/generated/prisma/client";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,18 +9,26 @@ type CategoryiconProps = {
 };
 
 export default function Categoryicon({ category }: CategoryiconProps) {
-  const params = useParams<{category: string}>()
- 
+  const params = useParams<{ category: string }>();
+
   return (
-    <div className={`${category.slug === params.category ? 'bg-blue-600 text-white': 'bg-slate-200' } flex items-center gap-5 w-full p-3 shadow  hover:bg-blue-400 transition-colors`}>
+    <div
+      className={`${category.slug === params.category ? "bg-blue-600 text-white" : "bg-slate-200"} flex items-center gap-5 w-full p-3 shadow  hover:bg-blue-400 transition-colors`}
+    >
       <div className="relative size-16 ">
-        <Image 
-        src={`/icon_${category.slug}.svg`}
-        alt={`Imagen de la categoria: ${category.name}`}
-        fill
+        <Image
+          src={`/icon_${category.slug}.svg`}
+          alt={`Imagen de la categoria: ${category.name}`}
+          fill
+          priority
         />
       </div>
-      <Link href={`/order/${category.slug}`} className="text-xl font-bold  p-2 w-full rounded">{category.name}</Link>
+      <Link
+        href={`/order/${category.slug}`}
+        className="text-xl font-bold  p-2 w-full rounded"
+      >
+        {category.name}
+      </Link>
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,21 +4,43 @@ import { Product } from "../generated/prisma/client";
 
 interface Store {
   order: OrderItem[];
-   AddToOrder: (product: Product) => void
+  AddToOrder: (product: Product) => void;
 }
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
-  AddToOrder: (product) =>{
-    const{categoryId, image, ...data} = product
+  AddToOrder: (product) => {
+    const { categoryId, image, ...data } = product;
 
-    console.log(data)
+    //Variable temporal donde agregarmos los productos
+    let order: OrderItem[] = [];
 
-    set((state)=>({
-      order:[...state.order,{
-        ...data,
-        quantity: 1,
-        subtotal: 1* product.price
-      }]
-    }))
-  }
+    //Si ya esta en el carrito
+    if (get().order.find((item) => item.id === product.id)) {
+      order = get().order.map((item) =>
+        item.id === product.id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: item.price * (item.quantity + 1),
+            }
+          : item,
+      );
+    } 
+    // Si no esta en el carrito 
+    else {
+      order = [
+        ...get().order,
+        {
+          ...data,
+          quantity: 1,
+          subtotal: 1 * product.price,
+        },
+      ];
+    }
+
+    // lo agregamos al state General 
+    set(() => ({
+      order: order,
+    }));
+  },
 }));
