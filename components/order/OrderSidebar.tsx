a5c71d7fd1419cc060c17506b13d80b86e284883
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-07 13:46:11 -0800

    Creando rutas y layout para Administrador

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,6 +1,7 @@
 // Abrimos la conexion a la bd
 import prisma from "@/src/lib/prisma";
 import Categoryicon from "../ui/Categoryicon";
+import Logo from "../ui/Logo";
 
 async function getCategories() {
   return await prisma.category.findMany();
@@ -14,7 +15,7 @@ export default async function OrderSidebar() {
 
   return (
     <aside className="md:w-72 md:h-screen bg-gray-100">
-      
+      <Logo />
       <nav className="mt-10 flex flex-col gap-4">
         {categories.map((category) => (
           <Categoryicon key={category.id} category={category} />
