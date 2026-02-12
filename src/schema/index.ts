f09a1123d645c60e4253950092ffcc7851a5ee19
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-12 09:58:03 -0800

    Se valido la creacion de productos

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -27,3 +27,20 @@ export const OrderIdSchema = z.object({
 export const SearchShema = z.object({
   search: z.string().trim().min(1, { message: "Rellena los campos" }),
 });
+
+
+export const ProductSchema = z.object({
+    name: z.string()
+        .trim()
+        .min(1, { message: 'El Nombre del Producto no puede ir vacio'}),
+    price: z.string()
+        .trim()
+        .transform((value) => parseFloat(value)) 
+        .refine((value) => value > 0, { message: 'Precio no válido' })
+        .or(z.number().min(1, {message: 'La Categoría es Obligatoria' })),
+    categoryId: z.string()
+        .trim()
+        .transform((value) => parseInt(value)) 
+        .refine((value) => value > 0, { message: 'La Categoría es Obligatoria' })
+        .or(z.number().min(1, {message: 'La Categoría es Obligatoria' })),
+})
