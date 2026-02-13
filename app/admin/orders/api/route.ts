b39b4066e6e0103cb687e4e3c0fecd6ece47ffe3
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-13 12:18:45 -0800

    configurando swr

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,9 +1,8 @@
 import prisma from "@/src/lib/prisma";
 //  Muestra datos en caché inmediatamente (stale)
-
 //  Hace una petición al servidor en segundo plano
-
 // Actualiza la UI cuando llegan los datos nuevos
+
 export async function GET() {
   const orders = await prisma.order.findMany({
     where: {
--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,54 +1,47 @@
+"use client";
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import prisma from "@/src/lib/prisma";
-import { revalidatePath } from "next/cache";
-import React from "react";
+import { OrderWhitProducts } from "@/src/types";
 
-async function getProducts() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
+
+export default function OrdersPage() {
+  const url = "/admin/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+
+  const { data, isLoading } = useSWR<OrderWhitProducts[]>(
+    url,
+    fetcher,
+    {
+      refreshInterval: 60000,
+      revalidateOnFocus: false
     },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
-  });
-  return orders;
-}
-
-export default async function OrdersPage() {
-  const orders = await getProducts();
-
-  //Actualizacion de la cache
-  const refreshOrders = async () => {
-    "use server";
-    revalidatePath("/admin/orders");
-  };
-
-  return (
-    <>
-      <Heading>Admistra Ordenes</Heading>
-      <form action={refreshOrders}>
-        <input
-          className="inline-flex max-w-60 items-center gap-2 px-4 py-2 bg-blue-600 rounded text-white hover:bg-blue-700 transition-colors"
-          type="submit"
-          value={"Actualizar Ordenes"}
-        />
-      </form>
-
-      {orders.length ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No Existen Ordenes Pendientes</p>
-      )}
-    </>
   );
+
+  if(isLoading) return (
+    <div className=" h-screen  flex justify-center items-center">
+<p className="text-2xl text-neutral-700">Cargando...</p>
+    </div>
+   
+  )
+
+  if (data)
+    return (
+      <>
+        <Heading>Admistra Ordenes</Heading>
+
+        {data.length ? (
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No Existen Ordenes Pendientes</p>
+        )}
+      </>
+    );
 }
