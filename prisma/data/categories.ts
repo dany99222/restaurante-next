baffea6c4ef14f0a3c2150c0eb5172da9ebcd705
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-02-14 18:29:49 -0800

    fix logo

--- prisma/data/categories.ts
+++ prisma/data/categories.ts
@@ -7,20 +7,6 @@ export const categories = [
         slug: "hamburguesa",
         name: "Hamburguesas"
       },
-      {
-        slug: "pizza",
-        name: "Pizzas"
-      },
-      {
-        slug: "dona",
-        name: "Donas"
-      },
-      {
-        slug: "pastel",
-        name: "Pasteles"
-      },
-      {
-        slug: "galletas",
-        name: "Galletas"
-      }
+      
+     
 ]
